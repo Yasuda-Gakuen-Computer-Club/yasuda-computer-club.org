abb37f831b7a2c5f399625dee3ae105d99468d1f
Fugaci <fugashi@yasuda-computer-club.org>
2018-11-28 19:10:22 +0900

    aboutの内容をmarkdownに

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,19 @@
 <template>
     <main>
         <PageTitle title="このクラブについて"/>
+        <div v-html="content"/>
     </main>
 </template>
 
 <script>
 import PageTitle from "@/components/PageTitle.vue";
+import content from "@/contents/static/about.md";
 
 export default {
-    components: { PageTitle }
+    components: { PageTitle },
+    data: () => ({
+        content
+    })
 };
 </script>
 
