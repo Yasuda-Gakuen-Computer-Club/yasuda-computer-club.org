5aebf33a6f3d0b27903f6f8365b2a9203c60cf62
Fugaci <fugashi@yasuda-computer-club.org>
2018-12-02 09:49:56 +0900

    articlesのindexページを作成

---
+++ pages/articles/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        <PageTitle title="記事一覧"/>
+    </main>
+</template>
+
+<script>
+import PageTitle from "@/components/PageTitle.vue";
+
+const context = require.context("@/contents/articles", false, /\.md$/);
+
+export default {
+    components: { PageTitle }
+};
+</script>
