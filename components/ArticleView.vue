7213547c4cf130efefe441e02d363bb4b9681e56
Fugaci <fugashi@yasuda-computer-club.org>
2018-12-02 19:17:23 +0900

    記事ビューをコンポーネント化

---
+++ components/ArticleView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <PageTitle :title="attributes.title"/>
+        <div
+            class="content markdown"
+            v-html="body"/>
+    </div>
+</template>
+
+<script>
+import PageTitle from "@/components/PageTitle.vue";
+
+export default {
+    name: "ArticleView",
+    components: { PageTitle },
+    props: {
+        attributes: {
+            type: Object,
+            required: true
+        },
+        body: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
